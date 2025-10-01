656e86e82d76ee90bef8f6f7fa24791afd9bf958
VortexWizrd <vortexwizrd@protonmail.com>
2025-10-01 15:54:53 -0700

    check scoresaber profile before linking

--- src/commands/beatsaber/link.ts
+++ src/commands/beatsaber/link.ts
@@ -9,47 +9,86 @@ module.exports = {
             option.setName('scoresaberid')
                 .setDescription('Your ScoreSaber ID (optional)')
                 .setRequired(false)),
+
     async execute(interaction: ChatInputCommandInteraction) {
-        const scoreSaberId = interaction.options.getString('scoresaberid');
+
+        const scoreSaberId = interaction.options.getString('scoresaberid')?.replace(/[^0-9]/g, '');
         
         const getBeatLeader = async () => {
+
             try {
+
                 const response = await fetch(`https://api.beatleader.com/player/discord/${interaction.user.id}`, {})
                 return response.json();
+
             } catch (error) {
+
                 console.error('Error fetching BeatLeader data:', error);
-                return await interaction.reply({ content: 'There was an error attempting to fetch BeatLeader data. Please try again later.', ephemeral: true });
+                await interaction.reply({ content: 'There was an error attempting to fetch BeatLeader data. Please try again later.', ephemeral: true });
+                return;
             }
         }
+
+        const getScoreSaber = async (id: string) => {
+
+            try {
+
+                const response = await fetch(`https://scoresaber.com/api/player/${id}/basic`);
+                return response.json();
+
+
+            } catch (error) {
+
+                await interaction.reply({ content: 'Invalid ScoreSaber ID', ephemeral: true });
+                return; 
+
+            }
+        }
+
         const beatLeaderData: any = await getBeatLeader();
 
-        const authorId = interaction.user.id;
+        const player = await Player.findOne({ discordId: interaction.user.id });
 
-        const query = { discordId: authorId };
-        const player = await Player.findOne(query);
         if (player) {
+
             if (beatLeaderData && beatLeaderData.id === player.beatLeaderId) {
+
                 if (scoreSaberId && scoreSaberId !== player.scoreSaberId) {
-                    player.scoreSaberId = scoreSaberId;
-                    await player.save();
-                    return await interaction.reply({ content: 'Your ScoreSaber ID has been updated successfully!', ephemeral: true });
+
+                    const scoreSaberProfile: any = await getScoreSaber(scoreSaberId);
+
+                    if (scoreSaberProfile && scoreSaberProfile.id === scoreSaberId) {
+
+                        player.scoreSaberId = scoreSaberId;
+                        await player.save();
+                        return await interaction.reply({ content: 'Your ScoreSaber ID has been updated successfully!', ephemeral: true });
+                        
+                    }
+
                 } else {
+
                     return await interaction.reply({ content: 'Your BeatLeader account is already linked!', ephemeral: true });
+
                 }
             } else {
+
                 player.beatLeaderId = beatLeaderData.id;
                 await player.save();
                 await interaction.reply({ content: 'Your BeatLeader account has been updated successfully!', ephemeral: true });
+
             }
         } else {
+
             const newPlayer = new Player({
-                discordId: authorId,
+                discordId: interaction.user.id,
                 beatLeaderId: beatLeaderData.id,
                 scoreSaberId: scoreSaberId || undefined,
                 scoreIds: []
             });
+
             await newPlayer.save();
             await interaction.reply({ content: 'Account(s) linked successfully!', ephemeral: true });
+
         }
         
     }
