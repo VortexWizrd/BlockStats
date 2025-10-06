cd83f5d6b9b051ef590d6f545bf9a35ec0abcd34
VortexWizrd <vortexwizrd@protonmail.com>
2025-10-05 19:41:28 -0700

    change cleanDatabase function

--- src/utils/cleanDatabase.ts
+++ src/utils/cleanDatabase.ts
@@ -1,13 +1,5 @@
 import Score from "../models/Score";
 
 export default async function cleanDatabase() {
-    const scores = await Score.find();
-    let i = 0;
-    for (const score of scores) {
-        if (score.beatLeaderData == null || score.beatLeaderData.timeset == null) {
-            Score.findOneAndDelete({ _id: score._id });
-            i++;
-        }
-    }
-    console.log("Removed " + i + " invalid scores!");
+    await Score.deleteMany({ beatLeaderData: null });
 }
