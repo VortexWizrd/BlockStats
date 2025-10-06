567ad3afe76ddb692be8c190192a5fdcb49a6a7a
VortexWizrd <vortexwizrd@protonmail.com>
2025-10-05 19:36:16 -0700

    change cleanDatabase function

--- src/utils/cleanDatabase.ts
+++ src/utils/cleanDatabase.ts
@@ -4,8 +4,9 @@ export default async function cleanDatabase() {
     const scores = await Score.find();
     let i = 0;
     for (const score of scores) {
-        if (!score.beatLeaderData || !score.beatLeaderData.timeset) {
+        if (score.beatLeaderData == null || score.beatLeaderData.timeset == null) {
             Score.deleteOne({ _id: score._id });
+            i++;
         }
     }
     console.log("Removed " + i + " invalid scores!");
